5adf5afbfe632004936f7a5254b148f0d985696a
Brew <TBREW@depaul.edu>
2019-09-25 21:22:12 -0500

    adjustsed loss conditions

--- game.js
+++ game.js
@@ -51,10 +51,10 @@ function draw()
   console.log(get(xpos, ypos)[1]);
   console.log('x: ' + xpos + ' y: ' + ypos);
 
-  if(get(xpos, ypos)[1] == 224) {
+  if((get(xpos, ypos)[1] == 224) || (get(xpos, ypos)[1] == 0)) {
     console.log('dead');
     xpos = windowWidth/2;
-    ypos = 50;
+    ypos = 30;
   }
   if(get(xpos, ypos)[1] == 209) {
     console.log('win');
