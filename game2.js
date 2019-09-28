3b6553f58f0a3f6e57450721fcaf4f347678f55f
tbrew1023 <tbrew1023@gmail.com>
2019-09-28 17:07:11 -0500

    added new levels and puzzles

--- game2.js
+++ game2.js
@@ -8,12 +8,14 @@ let currentLevel = 0;
 
 function setup() {
   levels.push(loadImage("./levels/lvl1.png"));
-  levels.push(loadImage("./levels/level2.png"));
+  levels.push(loadImage("./levels/lvl2.png"));
   levels.push(loadImage("./levels/level3.png"));
   levels.push(loadImage("./levels/level4.png"));
   levels.push(loadImage("./levels/level5.png"));
   levels.push(loadImage("./levels/level6.png"));
   levels.push(loadImage("./levels/level7.png"));
+  levels.push(loadImage("./levels/lvl3.png"));
+
   console.log(levels)
 
   new Image(levels[currentLevel], 0, 0, windowWidth, windowHeight);
