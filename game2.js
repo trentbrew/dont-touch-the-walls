5a6878d57049afbdec89f1159cb18f06afa8be63
tbrew1023 <tbrew1023@gmail.com>
2019-10-17 02:47:58 -0500

    splash free

--- game2.js
+++ game2.js
@@ -1,3 +1,4 @@
+
 var x, y, z;
 var xpos, ypos;
 var speed = 1;
@@ -7,23 +8,24 @@ let levels = [];
 let currentLevel = 0;
 let mousePositions = [];
 const MAX_POS = 15;
-var splashColor = '#e0e0e0';
-var outerDiam = 0;
-var blueHit = false;
-var purpleHit = false;
 
 function setup() {
-  levels.push(loadImage("./levels/invisible.png"));
-  levels.push(loadImage("./levels/invisible2.png"));
-  levels.push(loadImage("./levels/invisible3.png"));
-  levels.push(loadImage("./levels/invisible4.png"));
-  levels.push(loadImage("./levels/invisible4-2.png"));
-  levels.push(loadImage("./levels/invisible5.png"));
-  levels.push(loadImage("./levels/invisible5-2.png"));
-  levels.push(loadImage("./levels/invisible6.png"));
-  levels.push(loadImage("./levels/invisible6-2.png"));
-
-  //console.log(levels)
+  levels.push(loadImage("./levels/lvl1.png"));
+  levels.push(loadImage("./levels/lvl2.png"));
+  levels.push(loadImage("./levels/lvl2-2.png"));
+  levels.push(loadImage("./levels/lvl3.png"));
+  levels.push(loadImage("./levels/lvl3-2.png"));
+  levels.push(loadImage("./levels/lvl4.png"));
+  levels.push(loadImage("./levels/lvl4-2.png"));
+  levels.push(loadImage("./levels/lvl4-3.png"));
+  levels.push(loadImage("./levels/lvl5.png"));
+  levels.push(loadImage("./levels/lvl5-2.png"));
+  levels.push(loadImage("./levels/lvl5-3.png"));
+  levels.push(loadImage("./levels/lvl6.png"));
+  levels.push(loadImage("./levels/lvl6-2.png"));
+  levels.push(loadImage("./levels/lvl6-3.png"));
+  levels.push(loadImage("./levels/win.png"));
+  console.log(levels)
 
   new Image(levels[currentLevel], 0, 0, windowWidth, windowHeight);
 
@@ -40,81 +42,25 @@ function setup() {
 
 function draw() {
   // set background color to white
-  background('#e0e0e0');
-
-  //splash animation
-  for(var i = 0; i < 1; i+=1) {
-    var diam = outerDiam - 30 * i;    
-    if (diam > 0) {
-      var fade = map(diam, 0, width, 0, 255);
-      noStroke();
-      fill(splashColor);
-      ellipse(xpos, ypos, diam);
-    }
-  }
-  
-  outerDiam = outerDiam + 16;
-  
-  if(outerDiam > windowHeight * 1.2) {
-      //console.log('done');
-  }
-  else {
-    //console.log(outerDiam);
-  }
-
-  image(levels[currentLevel], 0, 0, windowWidth, windowHeight);
+  background(levels[currentLevel]);
 
   // add/subract xpos and ypos
   xpos = xpos - x;
   ypos = ypos + y;
 
-  //death cases
-
-  //
-  if((get(xpos, ypos)[0] == 255) /*white*/ || (get(xpos, ypos)[2] == 0) /*black*/) {
-    //console.log('dead');
+  if((get(xpos, ypos)[1] == 175) || (get(xpos, ypos)[0] == 255) || (get(xpos, ypos)[2] == 225) || (get(xpos, ypos)[2] == 0)) {
+    console.log('dead');
     xpos = windowWidth/2;
     ypos = 30;
   }
-
-  if((get(xpos, ypos)[1] == 102) /*purple gate*/) {
-    if(purpleHit == false) {
-      //console.log('dead');
-      xpos = windowWidth/2;
-      ypos = 30;
-    }
-  }
-
-  if((get(xpos, ypos)[2] == 248) /*blue gate*/) {
-    if(blueHit == false) {
-      //console.log('dead');
-      xpos = windowWidth/2;
-      ypos = 30;
-    }
-  }
-
   if((get(xpos, ypos)[2] == 161) /*green*/) {
-    //console.log('win');
+    console.log('win');
     xpos = windowWidth/2;
     ypos = 30;
     currentLevel++;
-    blueHit = false;
-    purpleHit = false;
-    splashColor = '#e0e0e0';
   }
-
-  //purple switch
-
-  if((get(xpos, ypos)[1] == 103)) {
-    purpleHit = true;
-    splash("#B866FE");
-  }
-
-  //blue switch
-
-  if((get(xpos, ypos)[2] == 249)) {
-    blueHit = true;
-    splash("#00C8F8");
+  if((get(xpos, ypos)[2] == 249) || (get(xpos, ypos)[2] == 250)) {
+    currentLevel++;
   }
 
   // draw ellipse
@@ -126,12 +72,17 @@ function draw() {
   //how you're drawing your pose
   ellipse(xpos, ypos, 15, 15);
   fill(0);
-
+  
+  //how you're storing the last 50 poses
   mousePositions.push({x: xpos, y: ypos});
+  
+  //removes poses that are older than 50
   if (mousePositions.length > MAX_POS) {
   	 mousePositions.shift();
   }
   for (let i = 0; i < mousePositions.length; i +=1) {
+    // how you want to draw the previous poses
+    // relate it to i to change pose drawing over time
   	ellipse(mousePositions[i].x, mousePositions[i].y, i, i);
   }
 
@@ -145,18 +96,3 @@ window.addEventListener('devicemotion', function(e)
   y = parseInt(e.accelerationIncludingGravity.y);
   z = parseInt(e.accelerationIncludingGravity.z);
 });
-
-//for debugging
-function mouseClicked() {
-  console.log('win');
-  xpos = windowWidth/2;
-  ypos = 30;
-  currentLevel++;
-  blueHit = false;
-  purpleHit = false;
-}
-
-function splash(color) {
-  outerDiam = 0;
-  splashColor = color;
-}
